bfed9f2ab2eece302f92c2349962765230579b46
isavin <caba1983@gmail.com>
2015-09-09 11:01:14 +0300

    trie implemented

---
+++ datastructures/src/isavin/datastructures/Trie.java
@@ -0,0 +1,55 @@
+package isavin.datastructures;
+
+/**
+ * Created by isavin on 09.09.2015.
+ */
+public class Trie {
+    private static class TrieNode {
+        private TrieNode[] children = new TrieNode[128];
+        private boolean leaf;
+    }
+
+    private TrieNode root = new TrieNode();
+
+    public Trie() {
+
+    }
+
+    public void addString(String string) {
+        TrieNode currentNode = root;
+        for (char c : string.toCharArray()) {
+            TrieNode nextNode = currentNode.children[c];
+            if (nextNode == null) {
+                nextNode = new TrieNode();
+                currentNode.children[c] = nextNode;
+            }
+            currentNode = nextNode;
+        }
+        currentNode.leaf = true;
+    }
+
+    public boolean contains(String string) {
+        TrieNode currentNode = root;
+        for (char c : string.toCharArray()) {
+            TrieNode nextNode = currentNode.children[c];
+            if (nextNode == null) {
+                return false;
+            }
+            currentNode = nextNode;
+        }
+        if (currentNode.leaf) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.addString("hello");
+        trie.addString("world");
+        System.out.println("Trie contains word \"hello\": " + trie.contains("hello"));
+        System.out.println("Trie contains word \"hell\": " + trie.contains("hell"));
+        System.out.println("Trie contains word \"world\": " + trie.contains("world"));
+        System.out.println("Trie contains word \"trie\": " + trie.contains("trie"));
+    }
+}

cde8feca7ac8f68484b998e11e67647905e9ca10
i-savin <caba1983@gmail.com>
2015-10-02 14:23:12 +0300

    subsets with reverse order added

--- tasks/src/isavin/tasks/combinatorics/Subset.java
+++ tasks/src/isavin/tasks/combinatorics/Subset.java
@@ -4,43 +4,63 @@ public class Subset {
 	
 	
 	public static void main(String[] args) {
-		int n = 5;
-		int k = 2;
+		int n = 20;
+		int k = 3;
 		int[] array = new int[n];
 		for (int i = 0; i < array.length; i++) {
 			array[i] = i + 1;
 		}
-		printSubsets(array, n, k);
-		//printSubsetsReverse(array, n, k);
+		//printSubsets(array, n, k);
+		printSubsetsReverse(array, n, k);
 	}
 	
+	/**
+	 * Печатает перестановки в прямом порядке
+	 */
 	private static void printSubsets(int[] array, int n, int k) {
 		int[] bits = new int[n];
 		for (int i = n - k; i < n; i++) {
 			bits[i] = 1;
 		}
-		//Sequence.print(array);
-		Sequence.print(bits);
-		//printArrayByMask(array, bits);
+		Sequence.print(array);
+		//Sequence.print(bits);
+		printArrayByMask(array, bits);
 		while (next(bits)) {
-			Sequence.print(bits);
-			//printArrayByMask(array, bits);
+			//Sequence.print(bits);
+			printArrayByMask(array, bits);
 		}
 	}
 
+	/**
+	 * Метод для поиска лексикографически следующей перестановки:
+	 * 00011 -> 00101 -> 00110 -> 01001 -> 01010 -> 01100 -> ... -> 11000
+	 * Возвращает true, если лексикографически следующая последовательность существует
+	 * и false - если текущая последовательность лексикографически последняя
+	 */
 	private static boolean next(int[] array) {
 		int s = array.length - 2;
+		//ищем первый элемент равный 0 такой чтобы следующий был равен 1
 		while (s >= 0 && !(array[s] == 0 && array[s+1] == 1)) {
 			s--;
 		}
 		if (s < 0) {
 			return false;
 		}
+		//s - индекс элемента, который нужно заменить на 1
+		//тогда для сохранения числа единиц нужно заменить один из 
+		//элементов справа от s на 0
+		//чтообы последовательность была лексикографически следующей,
+		//нужно после s сначала расположить все 0, которые были на участке [s, n)
+		//затем все 1, которые были на этом участке
 		int num = 0;
+		//считаем количество единиц на участке [s, n)
 		for (int k = s; k < array.length; k++) {
 			num += array[k];
 		}
 		array[s] = 1;
+		// (n - s - num) - количество нулей на участке на участке [s, n)
+		//тогда (s + (n - s - num)) = (n - num) - индекс последнего элемента, который
+		//в новой перестановке должен быть равен 1
 		for (int k = s + 1; k < array.length - num + 1; k++) {
 			array[k] = 0;
 		}
@@ -50,46 +70,63 @@ public class Subset {
 		return true;
 	}
 	
+	/**
+	 * Печатает перестановки в обратном порядке
+	 */
 	private static void printSubsetsReverse(int[] array, int n, int k) {
 		int[] bits = new int[n];
 		for (int i = 0; i < k; i++) {
 			bits[i] = 1;
 		}
-		//Sequence.print(array);
-		Sequence.print(bits);
-		//printArrayByMask(array, bits);
+		Sequence.print(array);
+		//Sequence.print(bits);
+		printArrayByMask(array, bits);
 		while (previous(bits)) {
-			Sequence.print(bits);
-			//printArrayByMask(array, bits);
+			//Sequence.print(bits);
+			printArrayByMask(array, bits);
 		}
 	}
 	
+	/**
+	 * Метод для поиска лексикографически предыдущей перестановки:
+	 * 11000 -> 10100 -> 10010 -> 10001 -> 01100 -> ... -> 00011
+	 * Возвращает true, если лексикографически предыдущая последовательность существует
+	 * и false - если текущая последовательность лексикографически первая
+	 */
 	private static boolean previous(int[] mask) {
-		//ищем первый справа элемент, равный 0, такой чтобы предыдущий был равен 1
-		int s = mask.length - 1;
-		while (s > 0 && !(mask[s] == 0 && mask[s-1] == 1)) {
+		//ищем первый справа элемент, равный 1, такой чтобы следующий был равен 0
+		int s = mask.length - 2;
+		while (s >= 0 && !(mask[s] == 1 && mask[s+1] == 0)) {
 			s--;
 		}
-		//System.out.println(s);
-		if (s == 0) {
+
+		if (s < 0) {//1
 			return false;
 		}
-		//s - индекс элемента, который нужно заменить на 1, тогда предыдущий нужно заменить на 0
-		int num = 0; //число единиц на участке [0 - s)
-		for (int i = 0; i < s - 1; i++) {
-			num += mask[i];
+		//s - индекс элемента, который нужно заменить на 0
+		//чтобы это сделать корректно, нужно один элемент справа от s заменить на 1
+		//чтобы полученная перестановка была лексикографически предыдущей,
+		//нужно после s сначала расположить все 1, затем оставшиеся 0:
+		int num = 0; //число единиц на участке [s - n)
+		for (int i = s; i < mask.length; i++) {
+			num += mask[i];//1
 		}
 		//System.out.println(num);
-		mask[s] = 1;
-		for (int i = 0; i < num; i++) {
+		mask[s] = 0;
+		//записываем в массив выисленное число единиц
+		for (int i = s + 1; i < s + num + 1; i++) {
 			mask[i] = 1;
 		}
-		for (int i = num; i < s; i++) {
+		//оставшийся массив до конца заполняем 0
+		for (int i = s + num + 1; i < mask.length; i++) {
 			mask[i] = 0;
 		}
 		return true;
 	}
 	
+	/**
+	 * Печатает массив по маске
+	 */
 	private static void printArrayByMask(int[] array, int[] mask) {
 		if (array.length != mask.length) {
 			throw new IllegalArgumentException("Array length and mask length are not equal");

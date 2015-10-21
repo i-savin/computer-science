3e7328ff7fd5501a57511b8a62da5c1ca789650b
i-savin <caba1983@gmail.com>
2015-10-21 22:29:25 +0300

    doubles added (wrong)

---
+++ tasks/src/isavin/tasks/backtracking/Doubles.java
@@ -0,0 +1,77 @@
+package isavin.tasks.backtracking;
+
+import isavin.tasks.combinatorics.Sequence;
+
+public class Doubles extends BacktrackingAbstract {
+    private int n;
+    private int[] source;
+    private int currentSize;
+
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println("n: " + n);
+
+        Doubles doubles = new Doubles(n);
+
+        doubles.proceed();
+    }
+
+    public Doubles(int n) {
+        this.n = n;
+        this.source = new int[n];
+        this.currentSize = 0;
+    }
+
+    @Override
+    protected void upAndWork() {
+        while(isUp()) {
+            up();
+        }
+        work();
+    }
+
+    @Override
+    protected void work() {
+        if (currentSize <= 2 || (currentSize == n && !(source[currentSize - 1] == source[currentSize - 2]
+            && source[currentSize - 2] == source[currentSize - 3]))) {
+            Sequence.print(source);
+            solutionsNumber++;
+        }
+    }
+
+    @Override
+    protected boolean isUp() {
+        // System.out.println(currentSize);
+        if (currentSize < 3) {
+            return true;
+        }
+        return (n >= 3 && currentSize < n && !(source[currentSize - 1] == source[currentSize - 2]
+                && source[currentSize - 2] == source[currentSize - 3]));
+    }
+
+    @Override
+    protected boolean isRight() {
+        return source[currentSize - 1] == 0;
+    }
+
+    @Override
+    protected boolean isDown() {
+        return currentSize > 0;
+    }
+
+    @Override
+    protected void up() {
+        currentSize++;
+    }
+
+    @Override
+    protected void down() {
+        source[currentSize - 1] = 0;
+        currentSize--;
+    }
+
+    @Override
+    protected void right() {
+        source[currentSize - 1]++;
+    }
+}
